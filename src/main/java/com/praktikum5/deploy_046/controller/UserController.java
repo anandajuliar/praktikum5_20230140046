d864f54b23ce7b94d470ed488afa2fcd860aebdb
anandajuliar <anandajulia001@gmail.com>
2026-04-07 14:09:51 +0700

    create user controller at controller package

---
+++ src/main/java/com/praktikum5/deploy_046/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.praktikum5.deploy_046.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+    private final String USERNAME = "admin";
+    private final String PASSWORD = "20230140046";
+
+    @GetMapping("/")
+    public String loginpage(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
+            Model model) {
+
+        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            return "home";
+        } else {
+            model.addAttribute("error", "Invalid username or password");
+        }
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
