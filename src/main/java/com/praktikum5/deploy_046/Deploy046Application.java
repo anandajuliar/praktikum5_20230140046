70c2ee9845e39889085043800d511aa6cdb358d3
anandajuliar <anandajulia001@gmail.com>
2026-04-07 14:09:19 +0700

    Initial commit

---
+++ src/main/java/com/praktikum5/deploy_046/Deploy046Application.java
@@ -0,0 +1,13 @@
+package com.praktikum5.deploy_046;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy046Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy046Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/praktikum5/deploy_046/Deploy046ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikum5.deploy_046;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy046ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
